0a498abd00f026cef19ec241e550e1b5657b3ab4
Geronimo9177 <geronimohur@gmail.com>
2025-02-09 11:19:48 -0500

    Recorrido V1.1

--- src/controller/controller/turtle_bot_player.py
+++ src/controller/controller/turtle_bot_player.py
@@ -12,13 +12,6 @@ class TurtleBotPlayer(Node):
         self.srv = self.create_service(UpdateFilename, 'update_filename', self.filename_callback)
         self.get_logger().info("TurtleBotPlayer iniciado y esperando archivos...")
 
-        # Variables para almacenar los comandos de velocidad
-        self.velocities = []
-        self.current_index = 0
-
-        # Temporizador para publicar los comandos de velocidad
-        self.timer = self.create_timer(0.03, self.publish_velocity)  # 0.03 segundos = 30 ms
-
     def filename_callback(self, request, response):
         file_path = request.filename
         self.get_logger().info(f"Recibido archivo: {file_path}")
@@ -26,29 +19,19 @@ class TurtleBotPlayer(Node):
         try:
             with open(file_path, 'r') as file:
                 lines = file.readlines()[1:]  # Omitir encabezado
-                self.velocities = [tuple(map(float, line.strip().split(','))) for line in lines]
-                self.current_index = 0
-                self.get_logger().info(f"Archivo cargado con {len(self.velocities)} comandos de velocidad.")
+                
+                for line in lines:
+                    vel_x, vel_z = map(float, line.strip().split(','))
+                    twist = Twist()
+                    twist.linear.x = vel_x
+                    twist.angular.z = vel_z
+                    self.cmd_vel_pub_.publish(twist)
+                    time.sleep(0.0275)  # Simula el envío pausado de comandos
         except Exception as e:
             self.get_logger().error(f"Error al leer el archivo: {e}")
             
         return response
 
-    def publish_velocity(self):
-        """ Publica el siguiente comando de velocidad en la lista """
-        if self.current_index < len(self.velocities):
-            vel_x, vel_z = self.velocities[self.current_index]
-            twist = Twist()
-            twist.linear.x = vel_x
-            twist.angular.z = vel_z
-            self.cmd_vel_pub_.publish(twist)
-            self.current_index += 1
-        else:
-            # Si ya se publicaron todos los comandos, detener el robot
-            twist = Twist()
-            twist.linear.x = 0.0
-            twist.angular.z = 0.0
-            self.cmd_vel_pub_.publish(twist)
 
 def main():
     rclpy.init()
